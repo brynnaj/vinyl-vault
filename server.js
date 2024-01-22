79969a09657c3076064139a8151825b7c85e0aa8
brynnaj <brynnaj78@gmail.com>
2024-01-22 16:05:48 -0500

    attempt to host backend

--- server.js
+++ server.js
@@ -7,28 +7,33 @@
 // app.use(express.json());
 
 // const db = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'password',
-//     database: 'eCommerce'
+//     host: 'sql5.freesqldatabase.com',
+//     user: 'sql5678943',
+//     password: 'qxyvTwSRBd',
+//     database: 'sql5678943'
 // });
 
+// // Route to check if the server is running
 // app.get('/', (req, res) => {
-//     return res.json("From Backend Side");
+//     return res.json("Server is running");
 // });
 
 // // Route to fetch all products
 // app.get('/products', (req, res) => {
-//     const sql = "SELECT id, name, description, price, image_url, artist FROM products";
+//     const sql = "SELECT id, name, description, price, image_url, artist, genre_id FROM products";
 //     db.query(sql, (err, data) => {
-//         if (err) return res.json(err);
+//         if (err) {
+//             console.error(err);
+//             return res.status(500).json({ error: 'Internal Server Error' });
+//         }
+
 //         return res.json(data);
 //     });
 // });
 
 
 // app.listen(8000, () => {
-//     console.log('listening');
+//     console.log('Server is listening on port 8000');
 // });
 
 const express = require('express');
@@ -64,7 +69,8 @@ app.get('/products', (req, res) => {
     });
 });
 
+const PORT = process.env.PORT || 8000; // Use process.env.PORT if available, otherwise use port 8000
 
-app.listen(8000, () => {
-    console.log('Server is listening on port 8000');
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
 });
